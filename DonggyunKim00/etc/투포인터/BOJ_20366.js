1c66ed6fc1b59f307baecbe81d613d185a94388c
donggyun <donggyunkim12@gmail.com>
2025-08-10 10:38:35 +0900

    코테 준비(2)

---
+++ DonggyunKim00/etc/투포인터/BOJ_20366.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath, 'utf8').toString().trim().split('\n');
+
+const N = Number(input[0]);
+const SNOW = input[1]
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b);
+
+const pairs = [];
+for (let i = 0; i < N - 1; i++) {
+  for (let j = i + 1; j < N; j++) {
+    pairs.push({ sum: SNOW[i] + SNOW[j], i, j });
+  }
+}
+
+pairs.sort((a, b) => a.sum - b.sum);
+
+console.log(pairs);
+
+let ans = Infinity;
+for (let k = 0; k < pairs.length - 1; k++) {
+  const p1 = pairs[k];
+  const p2 = pairs[k + 1];
+
+  // 두 쌍이 서로 겹치지 않을 때만 검사
+  if (p1.i !== p2.i && p1.i !== p2.j && p1.j !== p2.i && p1.j !== p2.j) {
+    const diff = Math.abs(p1.sum - p2.sum);
+    if (diff < ans) ans = diff;
+  }
+}
+
+console.log(ans);
